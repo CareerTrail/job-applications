df04b2dd3a9fa143caef442e74439630bcd7f316
DittmerOk <56869986+DittmerOk@users.noreply.github.com>
2024-05-22 13:09:01 +0300

    Update Registration.tsx
    fix: add formik

--- src/pages/Registration/Registration.tsx
+++ src/pages/Registration/Registration.tsx
@@ -6,29 +6,50 @@ import {
   TextField,
   Typography,
 } from "@mui/material";
-import { useState } from "react";
-import { useAddNewUserMutation } from "services/userApi"; // Укажите путь к вашему файлу с API
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import { useAddNewUserMutation } from "services/userApi";
 
 export const Registration = () => {
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
   const [addNewUser, { isLoading, isError, isSuccess }] =
     useAddNewUserMutation();
 
-  const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault();
-    try {
-      await addNewUser({ firstName, lastName, email, password }).unwrap();
-      setFirstName("");
-      setLastName("");
-      setEmail("");
-      setPassword("");
-    } catch (err) {
-      console.error("Failed to register user:", err);
-    }
-  };
+  const AddUserSchema = Yup.object().shape({
+    firstName: Yup.string()
+      .required("First Name is Required.")
+      .min(1, "First Name is Too Short."),
+    lastName: Yup.string()
+      .required("Last Name is Required.")
+      .min(1, "Last Name is Too Short."),
+    email: Yup.string().email().required("Email is Required."),
+    password: Yup.string()
+      .required("No password provided.")
+      .min(8, "Password is too short - should be 8 chars minimum.")
+      .matches(/(?=.*[0-9])/, "Password must contain a number."),
+  });
+
+  const formik = useFormik({
+    initialValues: {
+      firstName: "",
+      lastName: "",
+      email: "",
+      password: "",
+    },
+    validationSchema: AddUserSchema,
+    onSubmit: async (values) => {
+      try {
+        await addNewUser({
+          firstName: values.firstName,
+          lastName: values.lastName,
+          email: values.email,
+          password: values.password,
+        }).unwrap();
+        formik.resetForm();
+      } catch (err) {
+        console.error("Failed to register user:", err);
+      }
+    },
+  });
 
   return (
     <Container component="main" maxWidth="xs">
@@ -36,7 +57,7 @@ export const Registration = () => {
         <Typography component="h1" variant="h5">
           Sign up
         </Typography>
-        <form onSubmit={handleSubmit} noValidate>
+        <form onSubmit={formik.handleSubmit} noValidate>
           <Grid container spacing={2}>
             <Grid item xs={12} sm={6}>
               <TextField
@@ -48,8 +69,12 @@ export const Registration = () => {
                 id="firstName"
                 label="First Name"
                 autoFocus
-                value={firstName}
-                onChange={(e) => setFirstName(e.target.value)}
+                value={formik.values.firstName}
+                onChange={formik.handleChange}
+                error={
+                  formik.touched.firstName && Boolean(formik.errors.firstName)
+                }
+                helperText={formik.touched.firstName && formik.errors.firstName}
               />
             </Grid>
             <Grid item xs={12} sm={6}>
@@ -61,8 +86,12 @@ export const Registration = () => {
                 label="Last Name"
                 name="lastName"
                 autoComplete="lname"
-                value={lastName}
-                onChange={(e) => setLastName(e.target.value)}
+                value={formik.values.lastName}
+                onChange={formik.handleChange}
+                error={
+                  formik.touched.lastName && Boolean(formik.errors.lastName)
+                }
+                helperText={formik.touched.lastName && formik.errors.lastName}
               />
             </Grid>
             <Grid item xs={12}>
@@ -74,8 +103,10 @@ export const Registration = () => {
                 label="Email Address"
                 name="email"
                 autoComplete="email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
+                value={formik.values.email}
+                onChange={formik.handleChange}
+                error={formik.touched.email && Boolean(formik.errors.email)}
+                helperText={formik.touched.email && formik.errors.email}
               />
             </Grid>
             <Grid item xs={12}>
@@ -88,8 +119,12 @@ export const Registration = () => {
                 type="password"
                 id="password"
                 autoComplete="current-password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
+                value={formik.values.password}
+                onChange={formik.handleChange}
+                error={
+                  formik.touched.password && Boolean(formik.errors.password)
+                }
+                helperText={formik.touched.password && formik.errors.password}
               />
             </Grid>
           </Grid>

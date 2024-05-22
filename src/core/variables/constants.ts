0eef217e39e56db4be555410fb951455a331c0b5
DittmerOk <56869986+DittmerOk@users.noreply.github.com>
2024-05-22 13:28:40 +0300

    fix: delete comment and enum

--- src/core/variables/constants.ts
+++ src/core/variables/constants.ts
@@ -1,11 +1,6 @@
 export enum Pages {
-    main = '/',
-    auth = '/auth',
-    reg = '/reg',
-    applications = '/applications'
-  }
-
-  export enum UserErrorMessage {
-    invalidEmail = 'Email is not write'
-  }
-
+  main = "/",
+  auth = "/auth",
+  reg = "/reg",
+  applications = "/applications",
+}

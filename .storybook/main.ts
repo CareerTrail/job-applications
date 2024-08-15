d344c34544e320763d5b6728b76a4d7a808b3aa6
DittmerOk <56869986+DittmerOk@users.noreply.github.com>
2024-08-15 23:23:58 +0300

    fix: storybook_docs

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,6 +2,7 @@ import { StorybookConfig } from '@storybook/react-vite';
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.stories.@(js|jsx|ts|tsx|mdx)'],
+  addons: ['@storybook/addon-docs'],
   framework: {
     name: '@storybook/react-vite',
     options: {},

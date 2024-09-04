4f6db97aafa94bee13ac264d8f22144a96509022
DittmerOk <56869986+DittmerOk@users.noreply.github.com>
2024-09-04 16:30:24 +0300

    fix: add_adaptive

--- src/pages/Login/Login.styles.ts
+++ src/pages/Login/Login.styles.ts
@@ -1,58 +1,43 @@
 import styled, { createGlobalStyle } from 'styled-components';
 import { Colors } from 'core/variables/constants';
-import { Link } from 'react-router-dom';
 
-export const GlobalStyle = createGlobalStyle`
-  * {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-  }
-
-  body {
-    margin: 0;
-    font-family: Arial, sans-serif;
+export const GlobalStyle = createGlobalStyle`  
+body {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
   }
 `;
 
 export const Wrapper = styled.div`
-  position: relative;
-  overflow: hidden;
-  width: 100vw;
-  height: 100vh;
   display: flex;
-  align-items: center;
-  justify-content: space-between;
-
-  @media (max-width: 768px) {
-    flex-direction: column;
-    justify-content: center;
-    padding: 20px;
-  }
+  max-width: 1920px;
+  width: 100%;
+  min-height: 100vh;
+  position: relative;
 `;
 export const ImageContainer = styled.div`
-  position: relative;
-  width: 1334px;
-  height: 100%;
-  z-index: 2;
+  display: flex;
+  align-items: center;
   overflow: hidden;
+  width: 70%;
+  height: 100vh;
+  box-sizing: border-box;
+  padding: 20px 0 20px 20px;
+  position: absolute;
   img {
     width: 100%;
     height: 100%;
-    border-radius: 40px;
     object-fit: cover;
-    position: absolute;
-    padding: 20px 0 20px 20px;
+    border-radius: 40px;
+    position: relative;
   }
-  @media (max-width: 768px) {
-    width: 100%;
-    height: auto;
-    margin-right: 0;
-    margin-bottom: 20px;
+  @media (max-width: 1920px) {
+    width: 70%;
   }
 `;
 
-export const Title = styled.div`
+export const TitleImg = styled.div`
   font-family: 'onest', sans-serif;
   position: absolute;
   top: 80%;
@@ -118,86 +103,92 @@ export const Slider = styled.div`
 
 export const FormContainer = styled.div`
   position: relative;
-  width: 100%;
-  max-width: 400px;
-  max-height: 637px;
-  background: #fff;
+  margin-left: 70%;
   display: flex;
-  flex-direction: column;
   justify-content: center;
   align-items: center;
-  z-index: 1;
-  margin: auto auto;
+  box-sizing: border-box;
+  form {
+    max-width: 400px;
+    max-height: 637px;
+  }
+  @media (max-width: 1920px) {
+    width: 30%;
+  }
 `;
 
-export const Title1 = styled.div`
+export const Header = styled.div`
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+`;
+export const Title = styled.div`
   color: ${Colors.primary};
   font-size: 32px;
   font-weight: 700;
-  text-align: center;
 `;
 
-export const Title2 = styled.div`
+export const SubTitle = styled.div`
   color: ${Colors.secondary};
   font-size: 18px;
   font-weight: 400;
-  text-align: center;
-  margin-top: 16px;
-  margin-bottom: 8px;
 `;
 
-export const LaberForEmail = styled.label`
-  color: ${Colors.primary};
-  font-size: 18px;
-  margin-top: 16px;
-  display: block;
+export const Form = styled.div`
+  margin: 24px 0px;
+  label {
+    color: ${Colors.primary};
+    font-size: 18px;
+    display: block;
+  }
 `;
 
 export const AuthActions = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
-  margin-top: 20px;
-  margin-bottom: 24px;
-`;
-
-export const CheckBox = styled.input`
-  appearance: none;
-  background-color: ${Colors.bg_white};
-  border: 2px solid ${Colors.primary};
-  border-radius: 4px;
-  width: 15px;
-  height: 15px;
-  display: inline-block;
-  position: relative;
-  margin-top: 4px;
-  cursor: pointer;
-  &:checked {
-    background-color: ${Colors.bg_white};
-    border-color: ${Colors.accent};
-    &::before {
-      content: '';
-      position: absolute;
-      left: 2px;
-      width: 5px;
-      height: 7px;
-      border: 1px solid ${Colors.accent};
-      border-width: 0 2px 2px 0;
-      transform: rotate(45deg);
-    }
+  margin-top: 16px;
+  height: 20px;
+  a {
+    text-decoration: none;
+    color: ${Colors.accent};
+    font-size: 16px;
   }
 `;
 
-export const RememberMeLabel = styled.label`
-  margin-left: 20px;
-  font-size: 16px;
-  color: #4a4a4a;
-`;
-
-export const StyledLink = styled(Link)`
-  text-decoration: none;
-  color: ${Colors.accent};
-  font-size: 16px;
+export const RememberMe = styled.div`
+  display: flex;
+  label {
+    margin-left: 6px;
+    font-size: 16px;
+    color: #4a4a4a;
+  }
+  input {
+    appearance: none;
+    background-color: ${Colors.bg_white};
+    border: 2px solid ${Colors.primary};
+    border-radius: 4px;
+    width: 15px;
+    height: 15px;
+    display: inline-block;
+    position: relative;
+    margin-top: 4px;
+    cursor: pointer;
+    &:checked {
+      background-color: ${Colors.bg_white};
+      border-color: ${Colors.accent};
+      &::before {
+        content: '';
+        position: absolute;
+        left: 2px;
+        width: 5px;
+        height: 7px;
+        border: 1px solid ${Colors.accent};
+        border-width: 0 2px 2px 0;
+        transform: rotate(45deg);
+      }
+    }
+  }
 `;
 
 export const ActionToReg = styled.div`
@@ -206,13 +197,17 @@ export const ActionToReg = styled.div`
   align-items: center;
   text-align: center;
   padding-top: 16px;
-`;
-
-export const LaberForReg = styled.div`
-  color: ${Colors.primary};
-  font-size: 18px;
-  display: block;
-  padding-right: 5px;
+  a {
+    text-decoration: none;
+    color: ${Colors.accent};
+    font-size: 16px;
+  }
+  div {
+    color: ${Colors.primary};
+    font-size: 18px;
+    display: block;
+    padding-right: 5px;
+  }
 `;
 
 export const Body1 = styled.div`
